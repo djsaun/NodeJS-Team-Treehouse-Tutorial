e4e55cedcaf7745eebf9cfae9d328c381d74af20
David Saunders <david@monkee-boy.com>
2017-08-23 08:39:21 -0500

    Dynamically pull in user information from url

--- example-profile.js
+++
@@ -1,7 +0,0 @@
-const Profile = require("./profile.js");
-
-const studentProfile = new Profile("chalkers");
-
-studentProfile.on("end", console.dir);
-
-studentProfile.on("error", console.error);
--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+const Profile = require("./profile.js");
+
 //Handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
   //if url == "/" && GET
@@ -21,8 +23,32 @@ function user(request, response) {
   if (username.length > 0) {
     response.setHeader('Content-Type', 'text/plain');
     response.write('Header\n');
-    response.write(`${username}\n`);
-    response.end('Footer\n');
+
+    // get JSON
+    let studentProfile = new Profile(username);
+
+    // show profile on "end"
+    studentProfile.on("end", profileJSON => {
+
+      // Store values we need
+      const values = {
+        avatarUrl: profileJSON.gravatar_url,
+        username: profileJSON.profile_name,
+        badges: profileJSON.badges.length,
+        javascriptPoints: profileJSON.points.JavaScript
+      };
+
+      response.write(`${values.username} has ${values.badges} badges.\n`);
+      response.end('Footer\n');
+    });
+
+    // on "error"
+    studentProfile.on("error", error => {
+      response.write(`${error.message}\n`);
+      response.end('Footer\n');
+    });
+
+
   }
 
   //if url == "/...."
